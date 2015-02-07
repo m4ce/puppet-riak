2d8702ea67e6bccffcfa3e509e8a9b71bb414a50
Matteo Cerutti <matteo.cerutti@hotmail.co.uk>
2015-02-07 11:41:15 +0100

    setting should be delimited with dots rather than underscores

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,15 +8,15 @@ class riak::config {
 
   ini_setting {
     "riak_listener_http_internal":
-      setting => "listener_http_internal",
+      setting => "listener.http.internal",
       value => $::riak::listener_http_internal;
 
     "riak_listener_https_internal":
-      setting => "listener_https_internal",
+      setting => "listener.https.internal",
       value => $::riak::listener_https_internal;
 
     "riak_listener_protobuf_internal":
-      setting => "listener_protobuf_internal",
+      setting => "listener.protobuf.internal",
       value => $::riak::listener_protobuf_internal
   }
 }
