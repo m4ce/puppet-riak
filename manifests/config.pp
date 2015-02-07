874171cb3c6136055688116de3ca781670360088
Matteo Cerutti <matteo.cerutti@hotmail.co.uk>
2015-02-07 11:55:02 +0100

    ini_setting is unreliable since our parameters contain dots. Relying on file_line and our custom riak::setting define type.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,22 +1,12 @@
 class riak::config {
-  Ini_setting {
-    path => "/etc/riak/riak.conf",
-    section => "",
-    require => Package["riak"],
-    notify => Service["riak"]
-  }
+  riak::setting {
+    "listener.http.internal":
+      value => $::riak::listener_http_internal";
 
-  ini_setting {
-    "riak_listener_http_internal":
-      setting => "listener.http.internal",
-      value => $::riak::listener_http_internal;
-
-    "riak_listener_https_internal":
-      setting => "listener.https.internal",
+    "listener.https.internal":
       value => $::riak::listener_https_internal;
 
-    "riak_listener_protobuf_internal":
-      setting => "listener.protobuf.internal",
+    "riak.listener.protobuf.internal":
       value => $::riak::listener_protobuf_internal
   }
 }
---
+++ manifests/setting.pp
@@ -0,0 +1,11 @@
+define riak::setting ($setting = $title, $target = '/etc/riak/riak.conf', $value) {
+  $regex_escaped_setting = regsubst($setting, '\.', '\\.', 'G')
+
+  file_line {"riak_setting_${title}":
+    path  => $target,
+    line  => "${setting} = ${value}",
+    match => "^ *${regex_escaped_setting} *=.*$",
+    require => Package["riak"],
+    notify => Service["riak"]
+  }
+}
