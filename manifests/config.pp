2b0cabf22f8d3ba42b34bf0392019199fcf34e40
Matteo Cerutti <matteo.cerutti@hotmail.co.uk>
2015-02-07 13:11:24 +0100

    creating ssl dir

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,12 @@
 class riak::config {
+  file {$::riak::ssl_dir:
+    owner => "riak",
+    group => "riak",
+    mode => "0700",
+    ensure => "directory",
+    require => Package["riak"]
+  }
+
   riak::setting {
     "listener.http.internal":
       value => $::riak::listener_http_internal;
