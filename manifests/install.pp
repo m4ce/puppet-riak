62e5ddf61e17c5bf97f18c4834f58bf885837f79
Matteo Cerutti <matteo.cerutti@hotmail.co.uk>
2015-02-07 01:13:12 +0100

    adding descr attribute to yumrepo resource

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,5 +1,6 @@
 class riak::install {
   yumrepo {"basho":
+    descr => "basho_riak",
     baseurl => "https://packagecloud.io/basho/riak/el/${::operatingsystemmajrelease}/\$basearch",
     gpgcheck => 0,
     enabled => 1
