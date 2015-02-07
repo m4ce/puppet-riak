6de6ebd2ae96a2116002841365ccbb0e85661807
Matteo Cerutti <matteo.cerutti@hotmail.co.uk>
2015-02-07 09:20:30 +0100

    adding hiera configuration managing riak.conf as a template for now, later with file_line

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,2 +1,10 @@
 class riak::config {
+  # TODO: best would be to use file_line to manage this file
+  file {"/etc/riak/riak.conf":
+    owner => "riak",
+    group => "riak",
+    mode => "0644",
+    content => template("riak/riak.conf.erb"),
+    notify => Service["riak"]
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-class riak ($ensure, $enable) {
+class riak ($listener_http_internal, $listener_protobuf_internal, $listener_https_internal, $ensure, $enable) {
   contain riak::install
   contain riak::config
   contain riak::service
