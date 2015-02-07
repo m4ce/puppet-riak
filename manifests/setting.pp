3a39bdf71c43c8d4fc97d3c281d7485e3e88aa35
Matteo Cerutti <matteo.cerutti@hotmail.co.uk>
2015-02-07 17:15:35 +0100

    when value is empty the config parameter should just be absent

--- manifests/setting.pp
+++ manifests/setting.pp
@@ -2,9 +2,13 @@ define riak::setting ($setting = $title, $target = '/etc/riak/riak.conf', $value
   $regex_escaped_setting = regsubst($setting, '\.', '\\.', 'G')
 
   file_line {"riak_setting_${title}":
-    path  => $target,
-    line  => "${setting} = ${value}",
+    path => $target,
+    line => "${setting} = ${value}",
     match => "^ *${regex_escaped_setting} *=.*$",
+    ensure => $value ? {
+      "" => "absent",
+      default => "present",
+    },
     require => Package["riak"],
     notify => Service["riak"]
   }
